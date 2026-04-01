8c7ae7c1bb6e0257788f3e093a695125fa071614
BatakanDalkalach <arithus99@gmail.com>
2026-04-02 01:10:04 +0200

    Add partial views for PlotCard, MemberCard and StatsBar components

---
+++ WebApplication1-master/WebApplication1/Models/StatsBarItem.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1.Models
+{
+    public class StatsBarItem
+    {
+        public string Label { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public string? ColorClass { get; set; }
+    }
+}
