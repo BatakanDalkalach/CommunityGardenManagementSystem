fa61d7aee131f69139e8bacc63c34a302768c0cb
BatakanDalkalach <arithus99@gmail.com>
2026-04-06 02:32:19 +0200

    Replace AJAX dashboard with server-side rendering, remove HarvestApiController

--- WebApplication1-master/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1-master/WebApplication1/Controllers/HomeController.cs
@@ -1,13 +1,38 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.DatabaseContext;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly CommunityGardenDatabase _db;
+
+        public HomeController(CommunityGardenDatabase db)
         {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var total = await _db.GardenPlots.CountAsync();
+            var available = await _db.GardenPlots.CountAsync(p => !p.IsOccupied);
+
+            ViewBag.TotalPlots = total;
+            ViewBag.AvailablePlots = available;
+            ViewBag.OccupiedPlots = total - available;
+
+            var latestHarvests = await _db.HarvestRecords
+                .Include(h => h.SourcePlot)
+                .Include(h => h.Harvester)
+                .OrderByDescending(h => h.CollectionDate)
+                .Take(5)
+                .ToListAsync();
+
+            ViewBag.LatestHarvests = latestHarvests;
+
             return View();
         }
 
